1b088ce86f0bc247064e866850e9795f96baf2cc
Elvira Myroshnychenko <karashistka@gmail.com>
2018-05-21 11:05:20 +0300

    change order-service - add trading

--- order-service/index.js
+++ order-service/index.js
@@ -1,3 +1,24 @@
+/* 
+ORDERS data structure
+id
+market
+side
+creationTime
+receivedTime
+price
+volume
+remainingAmount
+executedAmount
+status
+----------------
+TRADES data structure
+id
+orderId
+slaveOrderId
+price
+volume
+*/
+
 const Hemera = require('nats-hemera')
 const HemeraJoi = require('hemera-joi')
 const HemeraJaeger = require('hemera-jaeger')
@@ -19,41 +40,110 @@ const knex = require('knex')({
 
 var Promise = require('bluebird');
 
-const createTrade(order, slave) {
-  return order;
-}
-
-const comparePrice = function(order, side, priceCompare, priceOrder) {
-  if (order.status !== "wait") {
-    return order
+const createTrade(order, slave, callback) {
+  var volume = 0;
+  if (order.remainingAmount <= slave.remainingAmount) {
+    volume = order.remainingAmount;
+    order.remainingAmount = 0;
+    order.status = "executed";
+    order.executedAmount += volume;
+    slave.remainingAmount -= volume;
+    slave.executedAmount += volume;
+  } else {
+    volume = slave.remainingAmount;
+    order.remainingAmount -= volume;
+    order.executedAmount += volume;
+    slave.remainingAmount = 0;
+    slave.status = "executed"
+    slave.executedAmount += volume;
   }
-  knex.select("*")
-    .from('orders')
-    .where('price', priceCompare, order.price)
-    .orWhere('price', '=', order.price)
-    .andWhere({'side': side, market: order.market, status: 'wait'})
-    .andWhere('createdAt', '<', order.creationTine)
-    .orderBy('price', priceOrder)
-    .limit(1)
-    .then(function(resp) {
-      if (resp.length != 0) {
-        createTrade(order, resp[0], function(response) {
-          order = response;
-          if (order.status !== "wait") {
-            return order
-          }
-          comparePrice(order, side, priceCompare, priceOrder, function(response) {
-            order = response;
-            if (order.status !== "wait") return order
-          })
-        })
-      } else {
-        return order
-      }
+  knex('trades').insert({
+    orderId: order.id,
+    slaveOrderId: slave.id,
+    volume: volume,
+    price: slave.price
+  })
+  .returning(['id', 'orderId', 'slaveOrderId', 'volume', 'price'])
+  .then(function(resp){
+    if (Array.isArray(order.trades)) {
+      order.trades.push(resp)
+    } else {
+      order.trades = [resp]
+    }
+    if (Array.isArray(slave.trades)) {
+      slave.trades.push(resp)
+    } else {
+      slave.trades = [resp]
+    }
+    knex('orders').where('id', '=', order.id).update({
+      status: order.status,
+      remainingAmount: order.remainingAmount,
+      executedAmount: order.executedAmount,
+      trades: order.trades
+    }).then(function(resp) {
+      knex('orders').where('id', '=', slave.id).update({
+        status: slave.status,
+        remainingAmount: slave.remainingAmount,
+        executedAmount: slave.executedAmount,
+        trades: slave.trades
+      }).then(function(resp) {
+        callback(null, order);
+      }).catch(function(err) {
+        callback(new Error("cannot update slave order after trade"), null)
+      });
+    }).catch(function(err) {
+      callback(new Error("cannot update order after trade"), null)
     })
+  })
+  .catch(function(err) {
+    callback(new Error("cannot create trade"), null)
+  })
+  callback(null, order);
 }
 
-const matchingOrder = function(order, trx) {
+const comparePrice = function(order, side, priceCompare, priceOrder, callback) {
+  if (order.status !== "wait") {
+    callback(null, order);
+  } else {
+    knex.select("*")
+      .from('orders')
+      .where('price', priceCompare, order.price)
+      .orWhere('price', '=', order.price)
+      .andWhere({'side': side, market: order.market, status: 'wait'})
+      .andWhere('createdAt', '<', order.creationTine)
+      .orderBy('price', priceOrder)
+      .limit(1)
+      .then(function(resp) {
+        if (resp.length != 0) {
+          createTrade(order, resp[0], function(err, response) {
+            if (err) {
+              callback(err, null)
+            } else {
+              order = response;
+              if (order.status !== "wait") {
+                callback(null, order);
+              } else {
+                comparePrice(order, side, priceCompare, priceOrder, function(err, response) {
+                  if (err) {
+                    callback(err, null);
+                  } else {
+                    order = response;
+                    callback(null, order);
+                  }
+                })
+              }
+            }
+          })
+        } else {
+          callback(null, order);
+        }
+      }).catch(function(err) {
+        callback(new Error("cannot select orders"), null);
+      })
+  }
+}
+
+const matchingOrders = function(order, trx, callback) {
   if (order.side == "buy") {
     var side = 'sell',
       priceCompare = '>',
@@ -63,8 +153,8 @@ const matchingOrder = function(order, trx) {
       priceCompare = '<=',
       priceOrder = 'asc'
   }
-  comparePrice(order, side, priceCompare, priceOrder, function(resp) {
-    return resp;
+  comparePrice(order, side, priceCompare, priceOrder, function(err, resp) {
+    callback(err, resp);
   })
 }
 
@@ -74,16 +164,23 @@ async function start() {
       var order = JSON.parse(request);
       order.receivedTime = new Date().toUTCString();
       order.remainingAmount = order.volume;
+      order.executedAmount = 0;
     } catch(err) {
       nats.publish(replyTo, JSON.stringify({err}))
     }
     knex.transaction(function(trx) {
       knex.insert(order).into('orders')
-        //.returning(['id', 'creationTime', 'status'])
+        .returning(['id', 'creationTime', 'status', 'price', 'market', 'volume', 'executedAmount', 'remainingAmount', 'side'])
         .transacting(trx)
         .then(function(resp) {
           var data = resp[0];
-          return matchingOrder(data, trx);
+          matchingOrders(data, trx, function(err, resp) {
+            if (err) {
+              return throw err
+            } else {
+              return resp;
+            }
+          });
         })
         .then(trx.commit)
         .catch(trx.rollback);
